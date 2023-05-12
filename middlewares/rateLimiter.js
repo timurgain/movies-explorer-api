d5bd16331b9046f4fbb6935d8638be0d7656cb24
Timur Gain <timur.gain@gmail.com>
2023-05-13 02:23:05 +0400

    fix: lint code

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -3,5 +3,5 @@ const rateLimiter = require('express-rate-limit');
 module.exports = rateLimiter({
   max: 5,
   windowMS: 5000, // 5 seconds
-  message: { message: 'You have exceeded the allowed request limit. Try again later.'},
+  message: { message: 'You have exceeded the allowed request limit. Try again later.' },
 });
